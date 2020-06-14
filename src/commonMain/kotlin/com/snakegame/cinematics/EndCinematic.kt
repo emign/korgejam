41e254e290952302a85fb8eaeabf7f6f048ef598
Adrian Guevara <adrianguevaralonso@gmail.com>
2020-06-14 23:45:29 +0200

    Added credits

--- src/commonMain/kotlin/com/snakegame/cinematics/EndCinematic.kt
+++ src/commonMain/kotlin/com/snakegame/cinematics/EndCinematic.kt
@@ -18,18 +18,24 @@ import kotlin.coroutines.CoroutineContext
 class EndCinematic(container: Container, private val player: Snake, private val coroutineContext: CoroutineContext, private val menu: ()->Unit) {
 
     lateinit var finalImg: Image
+    lateinit var credits: Image
     lateinit var fadeRect: SolidRect
+    var ended = false
+
     init {
         container.container {
             fadeRect = solidRect(1000.0, 1000.0, RGBA.float(0, 0, 0, 0))
             launch(coroutineContext) {
                 finalImg = image(resourcesVfs["level3/LarrioEnd.png"].readBitmap())
+                credits = image(resourcesVfs["level3/Credits.png"].readBitmap())
                 finalImg.alpha = 0.0
+                credits.alpha = 0.0
             }
 
             addFixedUpdater(MILLISECONDS_PER_FRAME, false) {
-                if(player.head.xpos > 8840){
+                if(player.head.xpos > /*8840*/ 100 && !ended){
                     fade()
+                    ended = true
                     player.cinematicMode = false
                     player.goRight = true
                 }
@@ -39,14 +45,16 @@ class EndCinematic(container: Container, private val player: Snake, private val 
 
     fun Container.fade(){
         launch(coroutineContext) {
-            fadeRect.show(0.5.seconds)
+            fadeRect.show(2.seconds)
             timeout(2.seconds) {
-                launch(coroutineContext) {
-                    finalImg.show(0.seconds)
-                }
+                finalImg.alpha = 1.0
 
-                timeout(2.seconds) {
-                    menu()
+                timeout(8.seconds) {
+                    credits.alpha = 1.0
+
+                    timeout(10.seconds) {
+                        menu()
+                    }
                 }
             }
         }
